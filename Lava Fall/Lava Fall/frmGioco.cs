142a911beeaed3752f19ffd2d86a4bfb3f411f81
TommySeb <79444403+TommySeb@users.noreply.github.com>
2022-04-24 15:58:03 +0200

    Update frmGioco.cs

--- Lava Fall/Lava Fall/frmGioco.cs
+++ Lava Fall/Lava Fall/frmGioco.cs
@@ -12,6 +12,13 @@ namespace Lava_Fall
 {
     public partial class FormGioco : Form
     {
+        #region Global costants
+        // Movement costants
+        const int MAXRIGHTFORMPOSITION = 650;
+        const int MAXLEFTFORMPOSITION = 0;
+        const int CHARACTERWIDTH = 145;
+        #endregion
+
         #region Global variables
         // Starting count
         int _counter = 3;
@@ -30,10 +37,9 @@ namespace Lava_Fall
         int _points = 0; // Point variable
         
         // Movement variables
-
-        bool jump;      // Direction of the move (up)
-        int initialForce = 50;    // Initial force of the jump
-        int force = 0;  // ?
+        bool jump;                // Indicates if the character is jumping
+        int initialForce = 75;    // Initial force of the jump
+        int force = 0;            // ?
 
         // VETTORE DI PROVA PER IL MIO ALGORITMO
         //(CONTIENE I PUNTI X DOVE PUO SPOWNARE LA BASE)
@@ -41,22 +47,22 @@ namespace Lava_Fall
         //791 / 4 = 197
         //ipotizzo una piattaforma lunga circa 200
 
-
-        int[] possiblePositions = { 10 , 220 ,430, 650  };
-
-        Point coordinates;
-        int _newY = 657;
+        // Array of possible position of objects (position x)
+        int[] defaultXPositions = { 10 , 220 ,430, 650  };
 
         #endregion
 
+        // FORM BUILDER
         public FormGioco()
         {
             InitializeComponent();
-
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
         }
 
-        #region Starting countdown
+        // STARTING COUNTDOWN - STATUS: RESOLVE BUGS
+        // Problems:
+        // 1 - Go isn't in the middle of the screen
+        // 2 (not important) - The background of the label isn't trasparent
         private void countDown_Tick(object sender, EventArgs e)
         {
             // Subtract 1 second to the counter 
@@ -83,13 +89,12 @@ namespace Lava_Fall
                 // Refresh counter label
                 lblCountDown.Text = _counter.ToString();
         }
-        #endregion
 
-        #region Increase points
+        // INCREASE POINTS - STATUS: OK
         /// <summary>
         /// Increases score of a set quantity
         /// </summary>
-        /// <param name="quantityToAdd"> Quantity to add to the points</param>
+        /// <param name="quantityToAdd"> Quantity to add to the points.</param>
         private void increasePoints(byte quantityToAdd)
         {
             // Add the quantity to the total points
@@ -98,9 +103,8 @@ namespace Lava_Fall
             // Refresh of the points label
             lblPunteggio.Text = _points.ToString();
         }
-        #endregion
-
-        #region Lava movement
+        
+        // MOVEMENT OF THE LAVA - STATUS: OK
         private void timerLava_Tick(object sender, EventArgs e)
         {
             // Set the new frame of the lava
@@ -114,76 +118,88 @@ namespace Lava_Fall
             else
                 i = 0;
         }
-        #endregion
 
-        // TODO Sistemare spostamento basi ottimizzando il codice che rallenta il programma
-        #region Spostamento basi
+        // MOVE OF THE BASIS - STATUS: OK
         private void spostamento_basi_Tick(object sender, EventArgs e)
         {
-            // each tick moves the base 10 px down
+            // For every tick move all the bases 10 pixels down
             pbBase1.Top += 10;
             pbBase2.Top += 10;
             pbBase3.Top += 10;
             pbBase4.Top += 10;
 
-            // procedure to restore the bases
-            RespownBases();
+            // If one of the basis is in the bottom of form respawn it in the top
+            RespawnBasesCheck();
 
             // Add 1 point to the points    
             increasePoints(1);
         }
 
-        private void RespownBases()
+        /// <summary>
+        /// Controls if one of the bases reached lava and, if true, brings it to the top of the form
+        /// </summary>
+        private void RespawnBasesCheck()
         {
+            // If the first base reached lava respawn it
             if (pbBase1.Top > 657)
-            {
-                coordinates = pbBase4.Location;
-                pbBase4.Location = new Point(possiblePositions[0], coordinates.Y - 668);
-
-            }
-            else if (pbBase4.Location.Y > 657)
-            {
-                coordinates = pbBase4.Location;
-                pbBase4.Location = new Point(possiblePositions[3], coordinates.Y - 668);
-                _newY += 650;
-            }
-            else if (pbBase2.Location.Y > 657)
-            {
-                coordinates = pbBase4.Location;
-                pbBase2.Location = new Point(possiblePositions[1], coordinates.Y - 668);
-
-            }
-            else if(pbBase3.Location.Y > 657)
-            {
-                coordinates = pbBase4.Location;
-                pbBase3.Location = new Point(possiblePositions[2], coordinates.Y - 668);
-
-            }
+                respawnObject(pbBase1);
+            // If the second base reached lava respawn it
+            else if (pbBase2.Top > 657)
+                respawnObject(pbBase2);
+            // If the third base reached lava respawn it
+            else if (pbBase3.Top > 657)
+                respawnObject(pbBase3);
+            // If the fourth base reached lava respawn it
+            else if (pbBase4.Top > 657)
+                respawnObject(pbBase4);
+            // If no bases reached lava exit
             else
                 return;
-
         }
-        #endregion
 
-        // Ok
-        #region Events if the user presses a key
+        /// <summary>
+        /// Respawns an object in the bottom of the form
+        /// </summary>
+        /// <param name="objectToMove"> Name of the PictureBox to move</param>
+        private void respawnObject(PictureBox objectToMove)
+        {
+            // Declaration of variables
+            int _randomNumber;
+            Random random = new Random();
+            // Generate a random number to choose the X position from the array of default X positions
+            _randomNumber = random.Next(0, defaultXPositions.Length - 1);
+            int X = defaultXPositions[_randomNumber];
+            // Calculate the Y position (take the Y position of the image and substract 668 to go to the top of the form
+            int Y = objectToMove.Location.Y - 668;
+            // Move the object in the top of the form with the calculated coordinates
+            objectToMove.Location = new Point(X, Y);
+        }
+
+        // MOVE OF THE CHARACTER (EVENT IF A KEY IS PRESSED AND LEFT-RIGHT MOVEMENT) - STATUS: OK
+        /// <summary>
+        /// Event if the user presses a key
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="key"></param>
         private void Form1_KeyDown(object sender, KeyEventArgs key)
         {
             // Perform an action based on the pressed key
             switch (key.KeyCode)
             {
-                // If the key is "left arrow" go back 20 px
+                // If the key is "left arrow" go back 20 px only if the character doesn't go outside the form
                 case Keys.Left:
-                    pbPersonaggio.Left -= 20;
+                    if(pbPersonaggio.Left - 20 >= MAXLEFTFORMPOSITION)
+                        pbPersonaggio.Left -= 20;
                     break;
-                // If the key is "right arrow" move forward 20 px
+                // If the key is "right arrow" move forward 20 px only if the character doesn't go outside the form
                 case Keys.Right:
-                    pbPersonaggio.Left += 20;
+                    if (pbPersonaggio.Left + 20 <= MAXRIGHTFORMPOSITION)
+                        pbPersonaggio.Left += 20;
                     break;
                 // Istructions if the key is "space"
                 case Keys.Space:
                     // Jump only if the character is not jumping
-                    if (jump == false)
+                    if (!jump)
                     {
                         jump = true;
                         force = initialForce;
@@ -191,9 +207,17 @@ namespace Lava_Fall
                     break;
             }
         }
-        #endregion
 
-        #region Character jump
+        // MOVE OF THE CHARACTER (JUMP) - STATUS: RESOLVE BUGS
+        // Problems:
+        // 1: Collision is not ok: The character automatically jumps
+        // 2: If the character is jumping on a base it won't jump with the space key
+
+        /// <summary>
+        /// Makes the character jump
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void characterJump(object sender, EventArgs e)
         {
             // Verifies if there's the necessity to jump
@@ -221,6 +245,5 @@ namespace Lava_Fall
                 jump = false;
             }
         }
-        #endregion
     }
 }
