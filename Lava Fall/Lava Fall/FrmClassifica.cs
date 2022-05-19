1ef22d37cf9fd064cedea906cf8cd8d379dee110
DreazyExe <92234192+DreazyExe@users.noreply.github.com>
2022-05-19 09:58:41 +0200

    Create FrmClassifica.cs

---
+++ Lava Fall/Lava Fall/FrmClassifica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lava_Fall
+{
+    public partial class FrmClassifica : Form
+    {
+        public FrmClassifica()
+        {
+            InitializeComponent();
+        }
+    }
+}
